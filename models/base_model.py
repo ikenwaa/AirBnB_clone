c9c8fcf2d68a47748b45426da6b00eaf4c59fc5e
ikenwaa <ajohn9353@gmail.com>
2023-02-12 05:53:34 -0800

    Added engine directory

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,44 @@
 #!/usr/bin/python3
-from datetime import datetime as dt
+from datetime import datetime
 from uuid import uuid4
 import models
 
 """
-BaseModel module
+Module BaseModel
+Parent of all classes
 """
 
 
 class BaseModel():
-    """
-    Base class for AirBnB project.
+    """Base class for Airbnb clone project
     Methods:
-        __init__(self, *args, **kwargs),
-        __str__(self),
-        __save__(self),
-        __repr__(self),
+        __init__(self, *args, **kwargs)
+        __str__(self)
+        __save(self)
+        __repr__(self)
         to_dict(self)
     """
 
     def __init__(self, *args, **kwargs):
-        """Initialize class attributes"""
+        """
+        Initialize attributes: random uuid, dates created/updated
+        """
         if kwargs:
             for key, val in kwargs.items():
                 if "created_at" == key:
-                    self.created_at = dt.strptime(kwargs["created_at",
-                        "%Y-%m-%dT%H:%M:%S.%f")
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
                 elif "updated_at" == key:
-                    self.updated_at = dt.strptime(kwargs["updated_at"],
-                        "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
                 elif "__class__" == key:
                     pass
                 else:
                     setattr(self, key, val)
         else:
             self.id = str(uuid4())
-            self.created_at = dt.now()
-            self.update_at = dt.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -56,7 +58,7 @@ class BaseModel():
         """
         Update instance with updated time & save to serialized file
         """
-        self.updated_at = dt.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
@@ -65,9 +67,9 @@ class BaseModel():
         """
         dic = {}
         dic["__class__"] = self.__class__.__name__
-        for key, val in self.__dict__.items():
-            if isinstance(val, (dt, )):
-                dic[key] = val.isoformat()
+        for k, v in self.__dict__.items():
+            if isinstance(v, (datetime, )):
+                dic[k] = v.isoformat()
             else:
-                dic[key] = val
+                dic[k] = v
         return dic
